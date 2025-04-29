06aad664b5ff6c8812f83f780cf1bb9d5312eb9e
fenilsoni <fsoni199@gmail.com>
2025-04-29 11:53:37 -0400

    Update script.js : added images

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function typeWriter() {
 }
 
 // Slideshow
-const images = ["images/image1.jpg", "images/image2.jpg", "images/image3.jpg"];
+const images = ["images/image1.jpg", "images/image2.jpg", "images/image3.jpg","images/image4.jpg","images/image5.jpg","images/image6.jpg","images/image7.jpg","images/image8.jpg","images/image9.jpg","images/image10.jpg","images/image11.jpg","images/image12.jpg","images/image13.jpg","images/image14.jpg","images/image15.jpg","images/image16.jpg","images/image17.jpg","images/image18.jpg","images/image19.jpg","images/image20.jpg","images/image21.jpg","images/image22.jpg","images/image23.jpg"];
 
 let currentImage = 0;
 
